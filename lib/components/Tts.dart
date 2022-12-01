463905d95c43248ba58431415dfc57b1bec66b88
LuXLu-9 <herobrine5501@gmail.com>
2022-12-01 20:48:19 +0700

    Add Tts

---
+++ lib/components/Tts.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class Tts extends StatelessWidget {
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+
+  speak(String text) async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              controller: textEditingController,
+            ),
+            ElevatedButton(
+              child: Text("Start Text To speech"),
+              onPressed: () => speak(textEditingController.text),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
