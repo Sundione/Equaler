6ba1c9c4970fc136b88ca426a4d0022b0bf2a482
Dol <94625226+Thanadol-Dol@users.noreply.github.com>
2024-08-04 13:50:48 +0700

    Update api_handler.dart

--- lib/models/api_handler.dart
+++ lib/models/api_handler.dart
@@ -34,11 +34,12 @@ class ApiHandler {
   }
 
   static Future<Map> getNewsMethod(List<String> parameter) async {
-    // Real: pub_124249313445c3671fd4d175e97415511a437
-    // Reserve: pub_13699438c8b63ce784777146929c925c17dbd
-    // Reserve2: pub_14276b8a4fbf8db4357d71da5be8e97b4a76b
+    static const String apiKey = String.fromEnvironment(
+      'API_KEY',
+      defaultValue: '',
+    );
     String rawUrl =
-        "https://newsdata.io/api/1/news?apikey=pub";
+        "https://newsdata.io/api/1/news?apikey=" + apiKey;
     // adding parameter to url for more specific query
     for (var par in parameter) {
       rawUrl = '$rawUrl&$par';
